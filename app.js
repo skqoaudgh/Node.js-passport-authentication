9256ff64b22b1fc257ed89220195719c4d63bbb6
myungho <skqoaudgh2@hanmail.net>
2019-12-25 16:51:36 +0900

    MongoDB 연동

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+const db = require('./config/keys').MongoURI;
 
 const indexRouter = require('./routes/index');
 const UserRouter = require('./routes/users');
 
 const app = express();
 
+mongoose.connect(db, { useUnifiedTopology: true, useNewUrlParser: true })
+    .then(() => console.log('MongoDB Connected..'))
+    .catch(err => console.log(err));
+
 app.use(expressLayouts);
 app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({ extended: false }));
 
 app.use('/', indexRouter);
 app.use('/users', UserRouter);
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MongoURI: 'mongodb+srv://Cada:asd123@node-rest-shop-zqnku.mongodb.net/test?retryWrites=true&w=majority'
+}
